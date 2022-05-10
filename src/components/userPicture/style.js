0ada7f27cef6380aecb335fbbc0e4f19fb5d1870
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-05-10 18:41:11 -0200

    Update style.js

--- src/components/userPicture/style.js
+++ src/components/userPicture/style.js
@@ -7,4 +7,8 @@ export const Container = styled.div`
 export const ProfilePicture = styled.img`
   width: 150px;
   border-radius: 50%;
+
+  @media scree and (max-width: 767px){
+    width: 100px;
+  }
 `
