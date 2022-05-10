164e1268650d3932b7129666eee0b936b25d104f
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-05-10 09:08:11 -0200

    Create style.js

---
+++ src/components/userDetails/style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+
+`
+
+export const Name = styled.h1`
+  margin-bottom: 1rem;
+`
+
+export const UserName = styled.h3`
+  margin-bottom: 1rem;
+  color: #ced4da;
+`
+
+export const Details = styled.p`
+
+`
