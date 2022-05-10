50075e1fcdf9bcd0ba901ad3d3f5e3e60515ecca
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-05-10 08:35:10 -0200

    Create style.js

---
+++ src/components/userNumbers/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  width: 480px;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  padding: 10px;
+  border-radius: .5rem;
+  background: #0096c7;
+  font-size: .85rem;
+  margin-bottom: 1.5rem;
+  margin-left: 1rem;
+`
+
+export const NumberContainer = styled.div`
+
+`
