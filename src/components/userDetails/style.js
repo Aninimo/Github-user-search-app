75eb879af9335e9c315671d14696942d7755092d
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-05-10 18:32:41 -0200

    Update style.js

--- src/components/userDetails/style.js
+++ src/components/userDetails/style.js
@@ -6,13 +6,23 @@ export const Container = styled.div`
 
 export const Name = styled.h1`
   margin-bottom: 1rem;
+  
+  @media screen and(max-width: 767px){
+   font-size: 1.2rem;
+  }
 `
 
 export const UserName = styled.h3`
   margin-bottom: 1rem;
   color: #ced4da;
+
+  @media screen and(max-width: 767px){
+    font-size: 1rem;
+  }
 `
 
 export const Details = styled.p`
-
+  @media screen and (max-width: 767ox){
+    font-size: .75rem;
+  }
 `
